6ac9051d84702bd83aef3743157b15f3e761398e
Saad Sufyan <saad.sufyan@spursol.com>
2023-07-12 20:02:57 +0500

    test commit

---
+++ Selenium/FunctionalTesting.cs
@@ -0,0 +1,60 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium
+{
+    class FunctionalTesting
+    {
+        IWebDriver driver;
+        [SetUp] 
+        public void SetUp()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Url = ("https://knoccsapp-staging.azurewebsites.net/accounts/login");
+
+        }
+
+        [Test]
+
+        public void dropdown()
+        {
+            driver.FindElement(By.CssSelector("input[id=Email]")).SendKeys("saadsufyan57@gmail.com");
+            driver.FindElement(By.Id("Password")).SendKeys("User@123");
+            driver.FindElement(By.CssSelector("button[type=submit")).Click();
+
+
+            driver.FindElement(By.XPath("//div[@class='kncs-header-options']/ul/li[3]/div/a")).Click();
+
+            driver.FindElement(By.XPath("//button[@data-target='#firstModal']")).Click();
+
+            //IWebElement dropdown = driver.FindElement(By.XPath("//app-company-create/div/div/div/div[2]/div/div/form/div/div[6]/mat-form-field/div/div/div/mat-select/div"));
+            //IWebElement dropdown = driver.FindElement(By.ClassName("ng-tns-c119-138"));
+
+            driver.FindElement(By.XPath("//app-company-create/div/div/div/div[2]/div/div/form/div/div[6]/mat-form-field/div/div/div/mat-select/div")).Click();
+            driver.FindElement(By.XPath("//mat-option[2]")).Click();
+            //SelectElement s = new SelectElement(dropdown);
+            //s.SelectByIndex(2);
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
--- Selenium/Locators.cs
+++ Selenium/Locators.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,9 @@ namespace Selenium
             //new WebDriverManager.DriverManager().SetUpDriver(new FirefoxConfig());
             driver = new ChromeDriver();
             driver.Manage().Window.Maximize();
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5); 
+
+            //implicit wait for 5 seconds if an element does not found
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
             driver.Url = "https://knoccsapp-staging.azurewebsites.net/accounts/login";
 
         }
@@ -33,25 +36,28 @@ namespace Selenium
             TestContext.Progress.WriteLine("Test has been started");
 
             driver.FindElement(By.CssSelector("input[id=Email]")).SendKeys("saadsufyan57@gmail.com");
-            driver.FindElement(By.Id("Password")).SendKeys("User@123");
+            driver.FindElement(By.Id("Password")).SendKeys("User@1234");
             driver.FindElement(By.CssSelector("button[type=submit")).Click();
 
+
+
             //Thread.Sleep(2000);
-            //String errorMessage = driver.FindElement(By.ClassName("loginError")).Text;
-            //TestContext.Progress.WriteLine(errorMessage);
+            String errorMessage = driver.FindElement(By.ClassName("loginError")).Text;
+            TestContext.Progress.WriteLine(errorMessage);
 
-            IWebElement dashboardUrl = driver.FindElement(By.XPath("//div[@class='kncs-header-options']/ul/li/div/a"));
+            //explicit wait - to wait for explicitly for a specific component
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions
+                .ElementToBeClickable(driver.FindElement(By.CssSelector("button[type=submit"))));
 
-            String dUrl = dashboardUrl.GetAttribute("href");
-            String expectedDashboarURL = "https://knoccsapp-staging.azurewebsites.net/dashboard";
+            //IWebElement dashboardUrl = driver.FindElement(By.XPath("//div[@class='kncs-header-options']/ul/li/div/a"));
 
-            Assert.AreEqual(expectedDashboarURL, dUrl);
+            //String dUrl = dashboardUrl.GetAttribute("href");
+            //String expectedDashboarURL = "https://knoccsapp-staging.azurewebsites.net/dashboard";
 
-
-
-
-            TestContext.Progress.WriteLine(dUrl);
-
+            //Assert.AreEqual(expectedDashboarURL, dUrl);
+            //TestContext.Progress.WriteLine(dUrl);
+            driver.Quit();
         }
 
     }
